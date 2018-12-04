f681bb8b715f75bffc876f240815efddf0c5ea37
MINZHANG WEI <81228741@qq.com>
2018-12-04 16:36:33 +0800

    update

--- fe/app.js
+++ fe/app.js
@@ -1,6 +1,13 @@
 //app.js
+const service = require('/utils/service');
 App({
-  onLaunch: function () {
+  onLaunch: function (r) {
+    service.userConfig(this);
+  },
+  onShow: function (r) {
+    if (r.shareTicket) {
+      service.getAndUploadGroup(r.shareTicket);
+    }
   },
   globalData: {
   }
---
+++ fe/utils/config.js
@@ -0,0 +1,11 @@
+/**
+ * 配置文件
+ */
+
+const config = {
+  host: 'https://api.pinche.mobi',
+  storage_ticket: 'user_ticket',// 本地存储ticket的名称
+  storage_userconfig: 'user_config',// 本地存储userConfig信息
+}
+
+module.exports = config
